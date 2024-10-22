9a3f633d653c7dd60c054f7e46e3995023e91edb
Rod Davison <exgno@exgnosis.ca>
2024-10-22 18:09:41 -0400

    labs

--- labs/spark-streaming/src/main/java/x/spark_streaming/WordCount1_Network.java
+++ labs/spark-streaming/src/main/java/x/spark_streaming/WordCount1_Network.java
@@ -4,9 +4,6 @@ package x.spark_streaming;
  $	spark-submit   --class  x.spark_streaming.NetworkWordCount  target/spark-streaming-1.0.jar
  */
 
-import java.util.Arrays;
-
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
@@ -21,45 +18,51 @@ public class WordCount1_Network {
 							 master("local[*]").
 							 getOrCreate();
 		spark.sparkContext().setLogLevel("ERROR");
-		
-		StreamingQuery query;
 
 		Dataset<Row> lines = spark.readStream()
 							      .format("socket")
 							      .option("host", "localhost")
 							      .option("port", 10000)
 				                  .load();
-
+		
 		// print out incoming data
-//		query = lines.writeStream()
-//				.outputMode("append")
-//				.format("console")
-//				.start();
+		StreamingQuery query = lines.writeStream()
+		  .outputMode("append")
+		  .format("console")
+		  .start();
 
-		// Split the lines into words
+		/*-
+		//TODO-1: Split the lines into words
 		Dataset<String> words = lines.as(Encoders.STRING())
 									 .flatMap((FlatMapFunction<String, String>) x -> 
-									 Arrays.asList(x.toLowerCase().split("\\s+")).iterator(), 
+									 Arrays.asList(x.split("\\s+")).iterator(), 
 									 Encoders.STRING());
 		
-		// TODO: You will notice the word count is case sensitive.
+		// TODO-2: You will notice the word count is case sensitive.
 		// so 'a' and 'A' are treated as different words
 		// fix the line above so words are treated case-insensitive
-		// Hint :  Arrays.asList(x.toLowerCase().split("\\s+")).iterator(), 
+		// Hint :  Arrays.asList(x.toLowerCase().split("\\s+")).iterator(),
+		 
+		// print out words data
+		StreamingQuery query = words.writeStream()
+		  .outputMode("append")
+		  .format("console")
+		  .start();
 
-//		query = words.writeStream()
-//				.outputMode("append")
-//				.format("console")
-//				.start();
+		*/
+		
+		/*- 
+                //# TODO-3: do word-count
 
 		// Generate running word count
 		Dataset<Row> wordCounts = words.groupBy("value").count();
 		
 		// Start running the query that prints the running counts to the console
-		query = wordCounts.writeStream()
+		StreamingQuery query = wordCounts.writeStream()
 		  .outputMode("complete")
 		  .format("console")
 		  .start();
+		*/
 
 		query.awaitTermination();
 		
--- labs/spark-streaming/src/main/java/x/spark_streaming/WordCount2_Kafka.java
+++ labs/spark-streaming/src/main/java/x/spark_streaming/WordCount2_Kafka.java
@@ -36,39 +36,55 @@ public class WordCount2_Kafka {
 							   .option("subscribe", "test").load();
 		
 		df.printSchema();
-
-                // StreamingQuery query = df.writeStream()
-                //                         .outputMode("append")
-                //                         .format("console")
-                //                         .start();
+		
+		// print out raw data
+		StreamingQuery query = df.writeStream()
+		  .outputMode("append")
+		  .format("console")
+		  .start();
+		
+		/*-
+		// TODO-1 : Convert raw bytes to string
 		
 		Dataset <Row> df2  = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
 		df2.printSchema();
-
-                // StreamingQuery query = df2.writeStream()
-                //                         .outputMode("append")
-                //                         .format("console")
-                //                         .start();
 		
+		StreamingQuery query = df2.writeStream()
+				  .outputMode("append")
+				  .format("console")
+				  .start();
+		*/
+		
+		/*-
+		 
+		// TODO-2: extract value
 		Dataset <String> df3 = df2.select("value").as(Encoders.STRING());
 		df3.printSchema();
-
-                // StreamingQuery query = df3.writeStream()
-                //                         .outputMode("append")
-                //                         .format("console")
-                //                         .start();
+		
+		// print out incoming data
+		StreamingQuery query = df3.writeStream()
+				  .outputMode("append")
+				  .format("console")
+				  .start();
+ 
+ */
+		
+		/*-
+		// TODO-3: break lines into words
 		
 		Dataset<String> words = df3.flatMap((FlatMapFunction<String, String>) s -> {
 			return  Arrays.asList(s.toLowerCase().split("\\s+")).iterator();
 		},
 				Encoders.STRING());
-
-                // StreamingQuery query = words.writeStream()
-                //                         .outputMode("append")
-                //                         .format("console")
-                //                         .start();
 		
-		// Generate running word count
+		StreamingQuery query = words.writeStream()
+				  .outputMode("append")
+				  .format("console")
+				  .start();
+		 */
+		
+		/*-
+		// TODO-4: Generate running word count
 		Dataset<Row> wordCounts = words.groupBy("value").count();
 		
 		// Start running the query that prints the running counts to the console
@@ -76,6 +92,7 @@ public class WordCount2_Kafka {
 				 				 .outputMode("complete")
 				                 .format("console")
 				                 .start();
+		 */
 
 		query.awaitTermination();
 
